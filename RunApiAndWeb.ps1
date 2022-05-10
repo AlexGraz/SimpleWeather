7ca73c68f8eb4a5594e3538851b3dfbdd389d5d5
Alex Graziani <alex.graziani@rpsgroup.com.au>
2022-05-10 12:41:13 +1000

    Added two scripts to run project

---
+++ RunApiAndWeb.ps1
@@ -0,0 +1,2 @@
+cd ./server/Api
+dotnet watch run --launch-profile 'API'
---
+++ RunTests.ps1
@@ -0,0 +1,3 @@
+cd ./server/Test
+dotnet test --logger:"console;verbosity=detailed"
+Read-Host -Prompt "Press Enter to exit"
